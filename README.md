b7a39559cb549e60ac91a9f6105ffadb69cb4813
avitori <zjr282138345@gmail.com>
2025-07-09 23:43:33 +0800

    feat: click to manage options

--- README.md
+++ README.md
@@ -1,29 +1,38 @@
 # sql_format
 
 ## 要求
+
 - Python 3.11+
 
 ## 安装
 
-1. 克隆本仓库到本地：
-  ```bash
-  git clone https://github.com/Avitori73/sql_format.git
-  cd sql_format
-  ```
+```bash
+# 克隆仓库
+git clone https://github.com/Avitori73/sql_format.git
 
-2. 使用 pip 以可编辑模式安装：
-  ```bash
-  pip install -e .
-  ```
+# 进入项目目录
+cd sql_format
+
+# 安装指令
+pip install -e .
+```
 
 ## 使用
 
-- 在控制台中运行以下命令，可以格式化剪贴板中的 SQL 语句，并重新复制到剪贴板中：
-  ```bash
-  fmt
-  ```
+1. 复制 SQL 语句到剪贴板。
+2. 在终端中运行以下命令：
 
-- 使用以下命令可以查看格式化后的 SQL 语句：
-  ```bash
-  fmt view
-  ```
+```bash
+fmt
+```
+
+3. 格式化后的 SQL 语句将被复制回剪贴板。
+4. 在代码中粘贴格式化后的 SQL 语句。
+
+## 可选参数
+
+- `-v` 或 `--view`：在终端中查看格式化后的 SQL 语句。
+
+```bash
+fmt -v
+```
--- src/fmt/main.py
+++ src/fmt/main.py
@@ -1,36 +1,44 @@
 import sqlparse
 import pyperclip
 import random
-import sys
+import click
+from rich.console import Console
+from rich.syntax import Syntax
+
+console = Console()
 
 
-def format_sql(raw):
-    formatted_sql = sqlparse.format(
-        raw,
-        reindent=True,  # 重新缩进
-        # reindent_aligned=True,  # 对齐缩进
-        keyword_case="upper",  # 关键词大写
-        indent_width=4,  # 缩进空格数
-        strip_comments=True,  # 删除注释
-    )
-    return formatted_sql
-
-
-def main():
+@click.command()
+@click.option("-v", "--view", is_flag=True, help="View formatted SQL in console")
+def main(view):
     # Get the raw SQL from the clipboard
     raw_sql = pyperclip.paste()
 
     # Format the SQL
-    formatted_sql = format_sql(raw_sql)
+    formatted_sql = format_sql(raw_sql.strip())
 
     # Copy the formatted SQL back to the clipboard
     pyperclip.copy(formatted_sql)
 
     # Check for -v argument
-    if "view" in sys.argv:
-        print(formatted_sql)
+    if view:
+        console.print(Syntax(formatted_sql, "sql", theme="github-dark", word_wrap=True))
+        console.print()
 
-    print(f"{get_random_emoji()} Formatted SQL copied to clipboard.")
+    console.print(
+        f"{get_random_emoji()} [cyan]Formatted SQL copied to clipboard.[/cyan]"
+    )
+
+
+def format_sql(raw):
+    return sqlparse.format(
+        raw,
+        reindent=True,  # 重新缩进
+        keyword_case="upper",  # 关键词大写
+        indent_width=4,  # 缩进空格数
+        strip_comments=True,  # 删除注释
+        # reindent_aligned=True,  # 对齐缩进
+    )
 
 
 def get_random_emoji():
